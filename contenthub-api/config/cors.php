94d6010f86ed3eacbb38f792b48ef64a81282fbf
Claudia Martin Aguilar <claudiamartinaguilar@icloud.com>
2026-04-20 14:00:45 +0100

    Fix CORS for Vercel

---
+++ contenthub-api/config/cors.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:4200',
+        'https://minicms-2anzy8eq0-claudias-projects-cbdc404b.vercel.app',
+        'https://minicms-umber.vercel.app'
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
